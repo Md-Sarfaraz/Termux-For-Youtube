d0c276ddc90b311e92b9ccbd81170aed81560dd0
Md Sarfaraz <sarfarazraj217@gmail.com>
2018-09-08 12:33:54 +0530

    Update youtube_settings.sh

--- youtube_settings.sh
+++ youtube_settings.sh
@@ -9,7 +9,7 @@ echo -e "Requesting access to storage\n"
 termux-setup-storage
 sleep 5
 
-echo -e "Installing youtube-dl\n"
+echo -e "Installing Dependencies\n"
 yes | pip install youtube-dl
 
 echo -e "Creating the Youtube folder to download the files\n"
@@ -22,8 +22,9 @@ echo -e "Creating bin folder\n"
 mkdir ~/bin
  
 echo -e "Downloading and installing termux-url-opener\n"
-curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/termux-url-opener -O ~/bin/termux-url-opener
+curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/termux-url-opener -o ~/bin/termux-url-opener
 sleep 1
+chmod ~/bin/termux-url-opener
 dos2unix ~/bin/termux-url-opener
 
 echo -e "\n"
