78371487cc7f54035f7d176ecbdaefd1e7c9a10a
Md Sarfaraz <sarfarazraj217@gmail.com>
2019-09-11 17:05:03 +0530

    Update youtube_settings.sh

--- youtube_settings.sh
+++ youtube_settings.sh
@@ -1,31 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "Updating default packages\n"
+echo "Updating default packages\n"
 apt update && apt -y upgrade
-echo -e "Installing Required Tools"
+echo "Installing Required Tools"
 apt install termux-api figlet python ffmpeg -y
 
-echo -e "Requesting access to storage\n"
+echo "Requesting access to storage\n"
 termux-setup-storage
 sleep 5
 
-echo -e "Installing Dependencies\n"
+echo "Installing Dependencies\n"
 yes | pip install youtube-dl
 
-echo -e "Creating the Youtube folder to download the files\n"
+echo "Creating the Youtube folder to download the files\n"
 mkdir ~/storage/shared/youtube
 
-echo -e "Creating youtube-dl folder for config\n"
+echo "Creating youtube-dl folder for config\n"
 mkdir -p ~/.config/youtube-dl
 
-echo -e "Creating bin folder\n"
+echo "Creating bin folder\n"
 mkdir ~/bin
  
-echo -e "Downloading and installing termux-url-opener\n"
+echo "Downloading and installing termux-url-opener\n"
 curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/termux-url-opener -o ~/bin/termux-url-opener
 sleep 1
 dos2unix ~/bin/termux-url-opener
 
-echo -e "\n"
-echo -e "Modified by :  \n"
+echo "\n"
+echo "Modified by :  \n"
 figlet RAJ
