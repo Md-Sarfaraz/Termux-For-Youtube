7f4259a718e0587d3d38728d96416c906ca1a28d
Md Sarfaraz <sarfarazraj217@gmail.com>
2019-10-11 19:11:27 +0530

    Mp3 Support

--- youtube_settings.sh
+++ youtube_settings.sh
@@ -1,31 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "Updating default packages\n"
+echo "Updating default packages"
 apt update && apt -y upgrade
-echo -e "Installing Required Tools"
+echo "Installing Required Tools"
 apt install termux-api figlet python ffmpeg -y
 
-echo "Requesting access to storage\n"
+echo "Requesting access to storage"
 termux-setup-storage
 sleep 5
 
-echo "Installing Dependencies\n"
+echo "Installing Dependencies"
 yes | pip install youtube-dl
 
-echo "Creating the Youtube folder to download the files\n"
+echo "Creating the Youtube folder to download the files"
 mkdir ~/storage/shared/youtube
 
-echo "Creating youtube-dl folder for config\n"
+echo "Creating youtube-dl folder for config"
 mkdir -p ~/.config/youtube-dl
 
-echo "Creating bin folder\n"
+echo "Creating bin folder"
 mkdir ~/bin
  
-echo "Downloading and installing termux-url-opener\n"
+echo "Downloading and installing termux-url-opener"
 curl https://raw.githubusercontent.com/SarfarazRLZ/Termux-For-Youtube/master/termux-url-opener -o ~/bin/termux-url-opener
-sleep 1
+sleep 2
 dos2unix ~/bin/termux-url-opener
 
-echo "\n"
-echo "Modified by :  \n"
+echo ""
+echo "Modified by :"
 figlet RAJ
